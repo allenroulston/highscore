7282d0096aa40d0c7bf20484d4f41e19317eb519
Allen Roulston <allen.roulston@gmail.com>
2016-07-26 17:07:19 -0400

    new stuff for welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
 
+  # what may explain the step by step to solving LOGOUT problems
+  # http://stackoverflow.com/questions/6946435/devise-why-doesnt-my-logout-link-work
   devise_for :users do
     get 'logout' => 'devise/sessions#destroy' # very important -ALLEN ROULSTON
   end
   
-  resources :high_scores
+  resources :high_scores do
+    collection do
+      get 'welcome'
+    end
+  end
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root to: "high_scores#index"
+  root to: "high_scores#welcome"
   
 end
