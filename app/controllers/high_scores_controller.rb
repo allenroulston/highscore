abd10b9f39246c10573e25db1d94b221135cd6ef
Allen Roulston <allen.roulston@gmail.com>
2016-07-26 17:07:03 -0400

    new stuff for welcome

--- app/controllers/high_scores_controller.rb
+++ app/controllers/high_scores_controller.rb
@@ -63,6 +63,9 @@ class HighScoresController < ApplicationController
     end
   end
 
+  def welcome
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_high_score
